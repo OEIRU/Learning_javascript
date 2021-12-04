64e3c6d5883e392e41b47b6970f11c82e31a916a
Vladimir <SpasiboMiryZaZemfiry@gmail.com>
2021-12-04 08:08:49 +0700

    Fourth lesson
    In this release, all actions have become functions, is it worth calling them with a capital letter?

--- script.js
+++ script.js
@@ -1,69 +1,73 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
+const PersonalMovieDB = {
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
+    privat: false,
+    start: function () {
+        PersonalMovieDB.count = +prompt('Сколько фильмов вы посмотрели?', '');
 
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
+        while (PersonalMovieDB.count == '' || PersonalMovieDB.count == null || isNaN(PersonalMovieDB.count)) {
+            PersonalMovieDB.count = +prompt('Сколько фильмов вы посмотрели?');
         }
+    },
+    RememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Название фильма', ''),
+                  b = prompt('Оцените его по 10 бальной шкале', '');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                PersonalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+
+    detectPersonalLevel: function () {
+        if (PersonalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (PersonalMovieDB.count >= 10 && PersonalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (PersonalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+
+    ShowMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(PersonalMovieDB);
+        }
+    },
+
+    ToggleVisibleMyDB: function () {
+        if (PersonalMovieDB.privat) {
+            PersonalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+
+    WriteYourGenres: function () {
+        for (let i = 1; i < 2; i++) {
+            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+
+            if (genres == '' || genres == null) {
+                console.log('Вы введи некорренетные данные или не ввели их вовсе');
+                i--;
+            } else {
+                PersonalMovieDB.genres = genres.split(', ');
+                PersonalMovieDB.genres.sort();
+            }
+        }
+        PersonalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });  
     }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-
-writeYourGenres();
+};
